373958000b7de3c2518c8b039b85487de22850bb
Federico Javier Martino <federicojaviermartino@gmail.com>
2023-08-27 15:41:15 +0200

    dto and entity

---
+++ src/peliculas/dto/create-pelicula.dto.ts
@@ -0,0 +1,9 @@
+export class CreatePeliculaDto {
+    name: string;
+    tagline: string;
+    first_brewed: string;
+    description: string;
+    attenuation_level: number;
+    brewers_tips: string;
+    contributed_by: string;
+}
